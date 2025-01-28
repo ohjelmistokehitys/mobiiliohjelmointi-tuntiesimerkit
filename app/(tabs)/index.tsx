3ef26258d5276f7f123dcfbd6bd60f501670be02
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-01-28 14:53:23 +0200

    Laskin historialla

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,39 @@
-import { Button, Text, TextInput, TextProps, View } from "react-native";
+import { Button, FlatList, Text, TextProps, View } from "react-native";
+import { useRef, useState } from "react";
 import styles from "../../components/ui/styles";
-import { useState } from "react";
+import NumberInput from "@/components/NumberInput";
 
-export const Title = ({ children }: TextProps) => <Text style={styles.title}>{children}</Text>
-export const Message = ({ children }: TextProps) => <Text style={styles.text}>{children}</Text>
+// These two custom components act as examples on how you could "wrap" built-in
+// components and make your own, reusable and semantically more meaningful ones.
+const Title = ({ children }: TextProps) => <Text style={styles.title}>{children}</Text>
+const StyledText = ({ children }: TextProps) => <Text style={styles.text}>{children}</Text>
 
-export default function Calculator() {
 
-    const [numA, setNumA] = useState("");
-    const [numB, setNumB] = useState("");
-    const [message, setMessage] = useState("");
+export default function CalculatorWithHistory() {
+    const [numA, setNumA] = useState(0);
+    const [numB, setNumB] = useState(0);
+    const [calculations, setCalculations] = useState<string[]>([]);
 
     const calc = (operator: "+" | "-") => {
-        const a = +numA;
-        const b = +numB;
-
-        if (Number.isNaN(a) || Number.isNaN(b)) {
-            setMessage("Input only numbers");
-            return;
-        }
-
-        if (operator === "+") {
-            setMessage(`${a} + ${b} = ${a + b}`);
-        } else {
-            setMessage(`${a} - ${b} = ${a - b}`);
-        }
+        const result = operator === "+" ? (numA + numB) : (numA - numB);
+        const calculation = `${numA} ${operator} ${numB} = ${result}`
+        setCalculations(calculations.concat(calculation));
     }
 
     return <View style={styles.container}>
-        <Title>Calculator 🧮</Title>
+        <Title>Calculator 🚀</Title>
 
-        <TextInput style={styles.textInput}
-            value={numA}
-            onChangeText={text => setNumA(text)}
-            placeholder="first number"
-            placeholderTextColor={styles.textInput.color}
-            keyboardType="numeric" />
-
-        <TextInput style={styles.textInput}
-            value={numB}
-            onChangeText={text => setNumB(text)}
-            placeholder="second number"
-            placeholderTextColor={styles.textInput.color}
-            keyboardType="numeric" />
+        <NumberInput value={numA} onChange={setNumA} placeholder="first number" />
+        <NumberInput value={numB} onChange={setNumB} placeholder="second number" />
 
         <View style={styles.buttonContainer}>
             <Button title="+" onPress={() => calc("+")} />
             <Button title="-" onPress={() => calc("-")} />
         </View>
-        <Message>{message}</Message>
+        <FlatList
+            data={calculations.toReversed()} // `toReversed` makes the most recent one appear on top
+            ListEmptyComponent={<StyledText>Let's do math!</StyledText>}
+            renderItem={({ item }) => <StyledText>{item}</StyledText>}
+        />
     </View>;
 }
