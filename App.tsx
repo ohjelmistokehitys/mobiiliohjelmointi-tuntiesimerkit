36dc1f6697a85a93033d0298187825885d58e12d
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-02-05 14:33:40 +0200

    Laskurin historian määrä lisätty valikkoon

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import CalculatorScreen from './screens/CalculatorScreen';
 import HistoryScreen from './screens/HistoryScreen';
 import { Text } from 'react-native';
-import { HistoryProvider } from './hooks/HistoryContext';
+import { HistoryProvider, useHistory } from './hooks/HistoryContext';
 
 
 
@@ -12,12 +12,17 @@ const { Navigator, Screen } = createBottomTabNavigator();
 export default function App() {
   return (
     <HistoryProvider>
-      <NavigationContainer>
-        <Navigator>
-          <Screen name="Calculator" component={CalculatorScreen} options={{ title: "Calculator", tabBarIcon: () => <Text>📱</Text> }} />
-          <Screen name="History" component={HistoryScreen} options={{ title: "History", tabBarIcon: () => <Text>📃</Text> }} initialParams={{ calculations: [] }} />
-        </Navigator>
-      </NavigationContainer>
+      <CalculatorApp />
     </HistoryProvider>
   );
+}
+
+function CalculatorApp() {
+  const { history } = useHistory();
+  return <NavigationContainer>
+    <Navigator>
+      <Screen name="Calculator" component={CalculatorScreen} options={{ title: "Calculator", tabBarIcon: () => <Text>📱</Text> }} />
+      <Screen name="History" component={HistoryScreen} options={{ title: `History (${history.length})`, tabBarIcon: () => <Text>📃</Text> }} initialParams={{ calculations: [] }} />
+    </Navigator>
+  </NavigationContainer>
 }
