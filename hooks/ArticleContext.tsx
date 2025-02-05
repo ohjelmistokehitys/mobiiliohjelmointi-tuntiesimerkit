cc0bb448bd33ef2b1871057281daf98a2effe871
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-02-05 11:42:22 +0200

    Async/await artikkelien hakemiseen

--- hooks/ArticleContext.tsx
+++ hooks/ArticleContext.tsx
@@ -17,18 +17,22 @@ export function ArticleProvider({ children }: { children: ReactNode }) {
     const [articles, setArticles] = useState<ArticleType[]>([]);
 
     useEffect(() => {
-        setLoading(true);
-
-        fetch("https://gist.githubusercontent.com/swd1tn002/c9f3379873a3fcba3db2fbeef8edbb66/raw/f61fd8c9561659ace8e9d23e45db679b79ac7e6e/articles.json")
-            .then(resp => {
+        const doFetch = async () => {
+            try {
+                setLoading(true);
+                const resp = await fetch("https://gist.githubusercontent.com/swd1tn002/c9f3379873a3fcba3db2fbeef8edbb66/raw/f61fd8c9561659ace8e9d23e45db679b79ac7e6e/articles.json")
                 if (!resp.ok) {
                     throw new Error(resp.status.toString());
                 }
-                return resp.json();
-            })
-            .then(data => setArticles(data))
-            .catch(e => Alert.alert(e))
-            .finally(() => setLoading(false));
+                const json = await resp.json();
+                setArticles(json);
+            } catch (e) {
+                console.error(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        doFetch();
     }, []);
 
     return <ArticleContext.Provider value={{ loading, articles }}>
